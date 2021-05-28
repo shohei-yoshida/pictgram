bf45b9f2fa51a0406199a11e89e6d4ba6e9a6fd6
名前 <>
2021-05-28 04:37:05 +0000

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,24 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: '登録が完了しました'
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
 
-def create
-  @user = User.new(name: params[:user][:name], email: params[:user][:email])
-  if @user.save
-    redirect_to root_path
-  else
-    render :new
-  end
-end
-
-def create
-  @user = User.new(name: params[:user][:name], email: params[:user][:email])
-  binding.pry
-  if @user.save
-    redirect_to root_path
-  else
-    render :new
-  end
+private
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
