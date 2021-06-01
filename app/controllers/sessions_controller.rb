a8f37306a8d7ff89699167f991042a522c08b2a8
名前 <>
2021-06-01 15:09:36 +0000

    fourth commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def email_params
+    params.require(:session).permit(:email)
+  end
+
+  def password_params
+    params.require(:session).permit(:password)
+  end
+end
